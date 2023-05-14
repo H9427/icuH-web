bb8b39ae762d2b4e50b1319d05f291e49534a98b
hbin <11323063@qq.com>
2023-05-14 20:02:01 +0800

    first-commit

---
+++ src/router/index.ts
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Index',
+    component: () => import('../views/Index.vue'),
+    // meta: {
+    //   showNavbar: true // 不显示导航栏
+    // }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: '404',
+    component: () => import('../views/404.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/Index.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- 左边小按钮 -->
+  <div class="navbar bg-base-100 flex-1">
+    <div class="navbar-start">
+      <div class="dropdown">
+        <label tabindex="0" class="btn btn-ghost btn-circle">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
+          </svg>
+        </label>
+        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+          <li><a>Homepage</a></li>
+          <li><a>xxxpage</a></li>
+        </ul>
+      </div>
+      <div>
+        <a class="btn btn-ghost normal-case text-xl" @click="this.$router.push('/login')">icuH</a>
+      </div>
+    </div>
+    <input type="text" placeholder="Search here" class="input input-bordered input-sm w-full max-w-xs bg-slate-50" />
+    <div class="navbar-end">
+      <a class="btn login" @click="this.$router.push('/login')">Login</a>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Index",
+  components: {},
+
+}
+</script>
+
+<style scoped>
+.login {
+  width: 110px;
+}
+</style>
